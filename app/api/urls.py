4581ad1857a18bef8c19108f73ef53e4d4de088d
Moussa Sidibe <sidibemousse@hotmail.com>
2024-12-26 03:06:26 +0000

    feat: add customer migration, and serializer

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from . import api_views
+from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path("users", api_views.get_users, name="get_users"),
-    path("products", api_views.get_products, name="get_products"),
-    path("users/add", api_views.add_user, name="add_user"),
-    path("users/update", api_views.update_user, name="update_user"),
-    path("users/delete", api_views.delete_user, name="delete_user"),
+    path("products", views.get_products, name="get.products"),
+    path("users", views.get_users, name="get.users"),
+    path("users/<str:email>/", views.edit_user, name="edit.user"),
+    path("users/add", views.add_user, name="add.user"),
+    path("users/update", views.update_user, name="update.user"),
+    path("users/delete", views.delete_user, name="delete.user"),
+    path("customers/", views.CustomerList.as_view(), name="customer.list"),
 ]
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
